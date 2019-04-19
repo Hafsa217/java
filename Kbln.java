e0f834e68574ff57027641f5f35a7513f70be7c2
Hafsa munawar <47898490+hafsa217@users.noreply.github.com>
2019-04-19 21:54:49 +0500

    kbln

---
+++ Kbln.java
@@ -0,0 +1,10 @@
+class Kbln {                                                                                                                 
+ public static void main(String args[])
+
+throw java.io.IOException {
+char ch;
+ System.out.print("press a key  folloed by enter; ");
+ch = (char) System.in.read ();
+ System.out.println("your key is ;"+ch);
+ } 
+   } 
