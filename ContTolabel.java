d1d425b73d64d25f77cc3116c7b327d4552db8ed
Hafsa munawar <47898490+hafsa217@users.noreply.github.com>
2019-04-20 18:17:33 +0500

    contlabel

---
+++ ContTolabel.java
@@ -0,0 +1,12 @@
+class ContTolabel {
+ public static void main(String args[]) {
+ outerloop:
+for(int i=1; i < 10; i++) {
+ System.out.println("\nouter loop pass" + i +", inner loop:");
+for( int j = 1; j < 10; j++) { 
+if(j==5) continue outerloop;
+System.out.print(j);
+ }
+}
+ }
+}
