711bb04d0db7473f7747f8839be86bb975017c08
Hafsa munawar <47898490+hafsa217@users.noreply.github.com>
2019-04-23 00:12:18 +0500

    operator

---
+++ Operator.java
@@ -0,0 +1,16 @@
+class Operator {
+
+public static void main(String args[]) {
+ int a = 20,  b = 10, c = 0, d = 20, e = 40, f = 30;
+boolean condition = true;
+ c = ++a;
+ System.out.println(" value of c(++a) = " +c);
+c = b++;
+System.out.println(" value of c is ;" + c);
+ c = --d;
+System.out.println("value of c (--d) = " +c);
+ c = --e;
+ System.out.println("value of c (--e) = " + c);
+   System.out.println("value of condition = " +  !condition);
+}
+ }
