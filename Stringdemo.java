ecd431d9d1d248aefaf8fd23432ba27ed7a670cc
Hafsa munawar <47898490+hafsa217@users.noreply.github.com>
2019-05-06 21:35:36 +0500

    string

---
+++ Stringdemo.java
@@ -0,0 +1,10 @@
+class Ftringdemo {
+public static void main(String args[]) {
+string str1 = new string (" java string are object");
+string str2 = "they are constructed in different ways" ;
+string str3 = new  string (str2);
+ System.out.println(str1);
+ System.out.println(str2);
+ system.out.println(str3);
+}
+}
